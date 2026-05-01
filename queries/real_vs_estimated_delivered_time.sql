8b6336fda0378e728f4aa393c7a381a83c449baf
Moni <monibasto@gmail.com>
2026-04-30 22:37:13 -0500

    modify query query_real_vs_estimated_delivery_time

--- queries/real_vs_estimated_delivered_time.sql
+++ queries/real_vs_estimated_delivered_time.sql
@@ -15,3 +15,46 @@
 -- 1. You can use the julianday function to convert a date to a number.
 -- 2. order_status == 'delivered' AND order_delivered_customer_date IS NOT NULL
 -- 3. Take distinct order_id.
+
+SELECT
+    STRFTIME('%m', o.order_purchase_timestamp ) AS month_no,
+    CASE STRFTIME('%m', o.order_purchase_timestamp)
+    WHEN '01' THEN 'Jan'
+    WHEN '02' THEN 'Feb'
+    WHEN '03' THEN 'Mar'
+    WHEN '04' THEN 'Apr'
+    WHEN '05' THEN 'May'
+    WHEN '06' THEN 'Jun'
+    WHEN '07' THEN 'Jul'
+    WHEN '08' THEN 'Aug'
+    WHEN '09' THEN 'Sep'
+    WHEN '10' THEN 'Oct'
+    WHEN '11' THEN 'Nov'
+    WHEN '12' THEN 'Dec' END AS month,
+    AVG(CASE WHEN STRFTIME('%Y', o.order_purchase_timestamp) = '2016' 
+        THEN julianday( o.order_delivered_customer_date)
+        - julianday(( o.order_purchase_timestamp)) END) AS Year2016_real_time,
+    AVG(CASE WHEN STRFTIME('%Y', o.order_purchase_timestamp) = '2017' 
+        THEN julianday( o.order_delivered_customer_date)
+        - julianday(( o.order_purchase_timestamp)) END) AS Year2017_real_time,
+    AVG(CASE WHEN STRFTIME('%Y', o.order_purchase_timestamp) = '2018' 
+        THEN julianday( o.order_delivered_customer_date)
+        - julianday(( o.order_purchase_timestamp)) END) AS Year2018_real_time,
+    AVG(CASE WHEN STRFTIME('%Y', o.order_purchase_timestamp) = '2016' 
+        THEN julianday( o.order_estimated_delivery_date)
+        - julianday(( o.order_purchase_timestamp)) END) AS Year2016_estimated_time,
+    AVG(CASE WHEN STRFTIME('%Y', o.order_purchase_timestamp) = '2017' 
+        THEN julianday( o.order_estimated_delivery_date)
+        - julianday((o.order_purchase_timestamp)) END) AS Year2017_estimated_time,
+    AVG(CASE WHEN STRFTIME('%Y', o.order_purchase_timestamp) = '2018' 
+        THEN julianday( o.order_estimated_delivery_date)
+        - julianday((o.order_purchase_timestamp)) END) AS Year2018_estimated_time
+FROM olist_orders o
+WHERE           
+    o.order_status = 'delivered'
+    AND o.order_delivered_customer_date IS NOT NULL
+GROUP BY
+    month_no, month
+ORDER BY        
+    month_no;
+
