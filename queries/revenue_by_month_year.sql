6c26e04aa3b1bff3a52c9961518d1ab9ff35b105
Moni <monibasto@gmail.com>
2026-05-01 20:08:15 -0500

    finalize all queries

--- queries/revenue_by_month_year.sql
+++ queries/revenue_by_month_year.sql
@@ -11,3 +11,38 @@
 -- 1. olist_order_payments has multiple entries for some order_id values. 
 -- For this query, make sure to retain only the entry with minimal payment_value for each order_id.
 -- 2. order_status == 'delivered' AND order_delivered_customer_date IS NOT NULL
+
+
+SELECT
+    STRFTIME('%m', o.order_delivered_customer_date) AS month_no,
+    --STRFTIME('%b', o.order_delivered_customer_date) AS month,
+    CASE STRFTIME('%m', o.order_delivered_customer_date)
+	    WHEN '01' THEN 'Jan'
+	    WHEN '02' THEN 'Feb'
+	    WHEN '03' THEN 'Mar'
+	    WHEN '04' THEN 'Apr'
+	    WHEN '05' THEN 'May'
+	    WHEN '06' THEN 'Jun'
+	    WHEN '07' THEN 'Jul'
+	    WHEN '08' THEN 'Aug'
+	    WHEN '09' THEN 'Sep'
+	    WHEN '10' THEN 'Oct'
+	    WHEN '11' THEN 'Nov'
+	    WHEN '12' THEN 'Dec' END AS month,
+    SUM(CASE WHEN STRFTIME('%Y', o.order_delivered_customer_date) = '2016' THEN p.payment_value ELSE 0 END) AS Year2016,
+    SUM(CASE WHEN STRFTIME('%Y', o.order_delivered_customer_date) = '2017' THEN p.payment_value ELSE 0 END) AS Year2017,
+    SUM(CASE WHEN STRFTIME('%Y', o.order_delivered_customer_date) = '2018' THEN p.payment_value ELSE 0 END) AS Year2018
+FROM olist_orders o 
+JOIN olist_order_payments p ON o.order_id = p.order_id
+/*JOIN (
+    SELECT order_id, MIN(payment_value) AS payment_value
+    FROM olist_order_payments
+    GROUP BY order_id
+) p ON o.order_id = p.order_id*/
+WHERE           
+    o.order_status = 'delivered'
+    AND o.order_delivered_customer_date IS NOT NULL
+GROUP BY
+    month_no, month
+ORDER BY        
+    month_no;   
--- queries/revenue_per_state.sql
+++ queries/revenue_per_state.sql
@@ -5,3 +5,20 @@
 
 -- HINT: 
 -- All orders should have a delivered status and the actual delivery date should be not null. 
+
+SELECT
+    c.customer_state,
+    SUM(p.payment_value) AS revenue 
+FROM olist_orders o
+    JOIN olist_customers c 
+         ON o.customer_id = c.customer_id
+    JOIN olist_order_payments p
+         ON o.order_id = p.order_id
+WHERE           
+    o.order_status = 'delivered'
+    AND o.order_delivered_customer_date IS NOT NULL
+GROUP BY
+    c.customer_state
+ORDER BY
+    revenue DESC                  
+
