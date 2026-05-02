b7ab30329f57d110040f69dbc3701b150b11a722
Moni <monibasto@gmail.com>
2026-05-01 19:28:29 -0500

    modify query global_amount_order_status

--- queries/global_amount_order_status.sql
+++ queries/global_amount_order_status.sql
@@ -2,3 +2,10 @@
 -- This query will return a table with two columns: order_status and Amount. 
 -- The first one will have the different order status classes 
 -- and the second one the total amount of each.
+SELECT 
+    o.order_status, 
+    count(o.order_id) AS Amount
+FROM 
+    olist_orders o
+GROUP BY 
+    o.order_status;       
